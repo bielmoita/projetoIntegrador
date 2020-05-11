27e0a65517ab91a529a8ba93daddca330da8d7de
Gabriel Fernando <oficial.gabriel89@gmail.com>
2020-05-11 14:35:33 -0300

    Transpilando do HTML padrão para o Angular - Projeto GETMOVEIS

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # projetoIntegrador
 Projeto Final Generation
+  
---
+++ getMoveis/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { FAQComponent } from './faq/faq.component';
+import { ContatoComponent } from './contato/contato.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    FAQComponent,
+    ContatoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

3730a8832457c56df2234882342107af994bfd42
thiagoagduarte <thiagoagduarte@gmail.com>
2020-06-25 16:29:29 -0300

    Implementação do modal no carrinho

--- getMoveis/src/app/home/home.component.ts
+++ getMoveis/src/app/home/home.component.ts
@@ -12,16 +12,36 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  cadeiras:string ="assets/img/home/items/cadeiras.png"
-  mesas:string ="assets/img/home/items/mesas.png"
-  sofa:string ="assets/img/home/items/sofa.png"
-  poltrona:string ="assets/img/home/items/poltrona.png"
+  cadeiras: string = "assets/img/home/items/cadeiras.png"
+  mesas: string = "assets/img/home/items/mesas.png"
+  sofa: string = "assets/img/home/items/sofa.png"
+  poltrona: string = "assets/img/home/items/poltrona.png"
 
   // Carroussel
 
-  carrossel1:string ="assets/img/home/carrossel/carrossel1.png"
-  carrossel2:string ="assets/img/home/carrossel/carrossel2.png"
-  
+  carrossel1: string = "assets/img/home/carrossel/carrossel1.png"
+  carrossel2: string = "assets/img/home/carrossel/carrossel2.png"
 
+  // Busca de produtos
+
+  buscaCadeiras() {
+    localStorage.setItem('nome', "cadeira");
+    location.assign('/produtos');
+  }
+
+  buscaMesas() {
+    localStorage.setItem('nome', "mesa");
+    location.assign('/produtos');
+  }
+
+  buscaPoltronas() {
+    localStorage.setItem('nome', "poltrona");
+    location.assign('/produtos');
+  }
+
+  buscaSofas() {
+    localStorage.setItem('nome', "sofa");
+    location.assign('/produtos');
+  }
 
 }
